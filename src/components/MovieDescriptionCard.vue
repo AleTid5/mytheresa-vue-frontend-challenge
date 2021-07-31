fd9f811a76aa1ad68df39b361283f35cd77ef569
atidele <alejandro.tidele@mercadolibre.com>
2021-07-31 10:35:25 -0300

    Movie details card little fix

--- src/components/MovieDescriptionCard.vue
+++ src/components/MovieDescriptionCard.vue
@@ -2,13 +2,13 @@
   <Card class-name="movie-card">
     <Image :src="movie.poster_path" />
     <div class="description">
-      <div class="stars">
+      <div class="stars" v-if="movie.vote_average">
         <StarIcon v-for="a in parseInt(movie.vote_average)" :key="a" />
-        <HalfStarIcon v-if="movie.vote_average % 2 !== 0" />
+        <HalfStarIcon v-if="movie.vote_average % 1 !== 0" />
         <sup>({{ movie.vote_average }})</sup>
       </div>
-      <div class="title">{{ movie.title }}</div>
-      <div class="overview">“ {{ movie.overview }} ”</div>
+      <div class="title" v-if="movie.title">{{ movie.title }}</div>
+      <div class="overview" v-if="movie.overview">“ {{ movie.overview }} ”</div>
       <Button type="blue" class-name="button-add-to-favorites"
         >Add to favorites</Button
       >
@@ -19,7 +19,6 @@
 <script>
 import HalfStarIcon from "@/assets/icons/HalfStarIcon";
 import StarIcon from "@/assets/icons/StarIcon";
-import MovieListApiService from "@/services/api/movie-list.api";
 import Card from "./Card";
 import Image from "./Image";
 import Button from "./Button";
@@ -27,15 +26,8 @@ import Button from "./Button";
 export default {
   name: "MovieDescriptionCard",
   components: { HalfStarIcon, StarIcon, Button, Image, Card },
-  data: () => ({
-    movie: {},
-  }),
-  created() {
-    (async () => {
-      this.movie = await MovieListApiService.getMovieById(
-        this.$route.params.movieId
-      );
-    })();
+  props: {
+    movie: Object,
   },
 };
 </script>
--- src/views/movie-details/index.vue
+++ src/views/movie-details/index.vue
@@ -3,17 +3,28 @@
     <router-link to="/">
       <Button>Go to cinema listings</Button>
     </router-link>
-    <MovieDescriptionCard />
+    <MovieDescriptionCard :movie="movie" />
   </div>
 </template>
 
 <script>
 import Button from "@/components/Button";
 import MovieDescriptionCard from "@/components/MovieDescriptionCard";
+import MovieListApiService from "@/services/api/movie-list.api";
 
 export default {
   name: "MovieDetailsView",
   components: { MovieDescriptionCard, Button },
+  data: () => ({
+    movie: {},
+  }),
+  created() {
+    (async () => {
+      this.movie = await MovieListApiService.getMovieById(
+        this.$route.params.movieId
+      );
+    })();
+  },
 };
 </script>
 
